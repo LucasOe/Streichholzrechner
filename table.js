dc41e1a87998f91b14068ac93fab8251545f8e30
LucasOe <lucas.oelker@gmx.de>
2019-05-12 20:04:37 +0200

    Fix Render / Add hovereffect

--- table.js
+++ table.js
@@ -1,32 +1,32 @@
 function createTable() {
   var tableDiv = document.getElementById("tableDiv");
-  var table = document.createElement('TABLE');
+  var table = document.createElement('table');
 
-  var tableBody = document.createElement('TBODY');
+  var tableBody = document.createElement('tbody');
 	tableBody.className = 'tableBody';
   table.appendChild(tableBody);
 
 	//Row
   for (var i = 0; i < 20; i++) {
-    var tr = document.createElement('TR');
+    var tr = document.createElement('tr');
 		tr.className = 'tr';
 		tr.id = i;
     tableBody.appendChild(tr);
 
 		//Column Speicherzelle
-    var td = document.createElement('TD');
+    var td = document.createElement('td');
     td.appendChild(document.createTextNode(i));
 		td.className = 'speicherzelle';
     tr.appendChild(td);
 
 		//Column Inhalt
-		var td = document.createElement('TD');
-		td.appendChild(document.createTextNode("0"); //Add user input here, select by clicking row.
+		var td = document.createElement('td');
+		td.appendChild(document.createTextNode("0")); //Add user input here, select by clicking row.
 		td.className = 'inhalt';
 		tr.appendChild(td);
 
 		//Column Bedeutung
-		var td = document.createElement('TD');
+		var td = document.createElement('td');
 		td.appendChild(document.createTextNode("0"));
 		td.className = 'bedeutung';
 		tr.appendChild(td);
@@ -34,13 +34,3 @@ function createTable() {
   tableDiv.appendChild(table);
 }
 createTable();
-
-var btns = tableBody.getElementsByClassName("tr");
-
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
