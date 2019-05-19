4e00b1ede11f035d634f56d53350fc768e3f5d70
LucasOe <lucas.oelker@gmx.de>
2019-05-19 16:33:30 +0200

    Fixes

--- app/update.js
+++ app/update.js
@@ -18,51 +18,53 @@ allInputs.forEach(function(button, buttonIndex) {
 		updateIsVar();
 
 		if(isVarArray[buttonIndex]) {
+			//TODO: Updatet erst wenn man die Zeile anklickt. Die nächste Zeile muss direkt geupdatet werden
 			bedeutung.textContent = input;
 			bedeutung.removeAttribute("title");
 		} else {
-		//Update Bedeutung
-		switch(input) {
-			case "1":
-				bedeutung.textContent = "Jmpg";
-				bedeutung.title = "Bedingter Sprung \n Sprung zur Adresse, die im Y-Register steht, wenn der Inhalt des X-Registers größer 0 ist.";
-				break;
-			case "2":
-				bedeutung.textContent = "Jmp";
-				bedeutung.title = "Unbedingter Sprung \n Sprung zu der Adresse, die im Y-Register steht.";
-				break;
-			case "3":
-				bedeutung.textContent = "Ld x";
-				bedeutung.title = "Lade einen Wert in das X-Register.";
-				break;
-			case "4":
-				bedeutung.textContent = "Ld y";
-				bedeutung.title = "Lade einen Wert in das Y-Register.";
-				break;
-			case "5":
-				bedeutung.textContent = "Add";
-				bedeutung.title = "Addition \n X-Register := X-Register + Y-Register";
-				break;
-			case "6":
-				bedeutung.textContent = "Sub";
-				bedeutung.title = "Subtraktion \n X-Register := X-Register – Y-Register";
-				break;
-			case "7":
-				bedeutung.textContent = "Str X";
-				bedeutung.title = "Store X \n Der Wert vom X-Register wird in die Speicherzelle geschrieben, deren Nummer im Y-Register steht.";
-				break;
-			case "8":
-				bedeutung.textContent = "Ld f";
-				bedeutung.title = "Load from \n Lädt den Inhalt der Speicherzelle in das X-Register, deren Nummer im Y-Register steht.";
-				break;
-			case "":
-				bedeutung.textContent = "0";
-				bedeutung.removeAttribute("title");
-				break
-			default:
-				bedeutung.textContent = input;
-				bedeutung.removeAttribute("title");
-		}}
+			//Update Bedeutung
+			switch(input) {
+				case "1":
+					bedeutung.textContent = "Jmpg";
+					bedeutung.title = "Bedingter Sprung \n Sprung zur Adresse, die im Y-Register steht, wenn der Inhalt des X-Registers größer 0 ist.";
+					break;
+				case "2":
+					bedeutung.textContent = "Jmp";
+					bedeutung.title = "Unbedingter Sprung \n Sprung zu der Adresse, die im Y-Register steht.";
+					break;
+				case "3":
+					bedeutung.textContent = "Ld x";
+					bedeutung.title = "Lade einen Wert in das X-Register.";
+					break;
+				case "4":
+					bedeutung.textContent = "Ld y";
+					bedeutung.title = "Lade einen Wert in das Y-Register.";
+					break;
+				case "5":
+					bedeutung.textContent = "Add";
+					bedeutung.title = "Addition \n X-Register := X-Register + Y-Register";
+					break;
+				case "6":
+					bedeutung.textContent = "Sub";
+					bedeutung.title = "Subtraktion \n X-Register := X-Register – Y-Register";
+					break;
+				case "7":
+					bedeutung.textContent = "Str X";
+					bedeutung.title = "Store X \n Der Wert vom X-Register wird in die Speicherzelle geschrieben, deren Nummer im Y-Register steht.";
+					break;
+				case "8":
+					bedeutung.textContent = "Ld f";
+					bedeutung.title = "Load from \n Lädt den Inhalt der Speicherzelle in das X-Register, deren Nummer im Y-Register steht.";
+					break;
+				case "":
+					bedeutung.textContent = "0";
+					bedeutung.removeAttribute("title");
+					break
+				default:
+					bedeutung.textContent = input;
+					bedeutung.removeAttribute("title");
+				}
+			}
 
 		//Setzt die Klasse von tr zu var wenn der vorherige Befehl Ld x/y ist.
 		tr.forEach(function(tr, trIndex) {
@@ -70,7 +72,7 @@ allInputs.forEach(function(button, buttonIndex) {
 			if(isVarArray[trIndex] === true) {
 				test.classList.add("var");
 			} else {
-				test.classList.remove("var");
+				if(test) test.classList.remove("var");
 			}
 		});
   }
