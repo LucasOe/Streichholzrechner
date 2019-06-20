b7a7c68e41236c429b9e930c70c01d5fdb857dce
LucasOe <lucas.oelker@gmx.de>
2019-06-20 12:47:00 +0200

    Add debug function initialize()

---
+++ app/debug.js
@@ -0,0 +1,13 @@
+function initialize() {
+	setCellContent(1, 3);
+	setCellContent(2, 5);
+	setCellContent(3, 4);
+	setCellContent(4, 6);
+	setCellContent(5, 5);
+	setCellContent(6, 4);
+	setCellContent(7, 20);
+	setCellContent(8, 7);
+	setCellContent(9, 4);
+	setCellContent(10, 0);
+	setCellContent(11, 2);
+}
