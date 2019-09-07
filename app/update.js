c04249e4d006876ba661a0bb659defe880ba631d
LucasOe <lucas.oelker@gmx.de>
2019-09-07 14:07:05 +0200

    Add & Fix titles

--- app/update.js
+++ app/update.js
@@ -56,11 +56,11 @@ function updateBedeutung(index) {
 		switch(input) {
 			case "1":
 				bedeutung.textContent = "Jmpg";
-				bedeutung.title = "Bedingter Sprung \n Sprung zur Adresse, die im Y-Register steht, wenn der Inhalt des X-Registers größer 0 ist.";
+				bedeutung.title = "Bedingter Sprung:\n Sprung zur Adresse, die im Y-Register steht, wenn der Inhalt des X-Registers größer 0 ist.";
 				break;
 			case "2":
 				bedeutung.textContent = "Jmp";
-				bedeutung.title = "Unbedingter Sprung \n Sprung zu der Adresse, die im Y-Register steht.";
+				bedeutung.title = "Unbedingter Sprung:\n Sprung zu der Adresse, die im Y-Register steht.";
 				break;
 			case "3":
 				bedeutung.textContent = "Ld x";
@@ -72,19 +72,19 @@ function updateBedeutung(index) {
 				break;
 			case "5":
 				bedeutung.textContent = "Add";
-				bedeutung.title = "Addition \n X-Register := X-Register + Y-Register";
+				bedeutung.title = "Addition:\n X-Register := X-Register + Y-Register";
 				break;
 			case "6":
 				bedeutung.textContent = "Sub";
-				bedeutung.title = "Subtraktion \n X-Register := X-Register – Y-Register";
+				bedeutung.title = "Subtraktion:\n X-Register := X-Register – Y-Register";
 				break;
 			case "7":
 				bedeutung.textContent = "Str X";
-				bedeutung.title = "Store X \n Der Wert vom X-Register wird in die Speicherzelle geschrieben, deren Nummer im Y-Register steht.";
+				bedeutung.title = "Store X:\n Der Wert vom X-Register wird in die Speicherzelle geschrieben, deren Nummer im Y-Register steht.";
 				break;
 			case "8":
 				bedeutung.textContent = "Ld f";
-				bedeutung.title = "Load from \n Lädt den Inhalt der Speicherzelle in das X-Register, deren Nummer im Y-Register steht.";
+				bedeutung.title = "Load from:\n Lädt den Inhalt der Speicherzelle in das X-Register, deren Nummer im Y-Register steht.";
 				break;
 			case "":
 				bedeutung.textContent = "0";
